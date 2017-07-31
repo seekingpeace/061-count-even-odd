bc31fe1270be0c64179448d4b21fb499a93eaf08
seekingpeace <seekingpeace@users.noreply.github.com>
2017-07-31 21:34:03 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,9 @@
-def solution(list_of_nums):
-    """Enter Code Here"""
+def solution(l):
+    e=len(list(filter(lambda x:x%2==0,l)))
+    o=len(l)-e
+    return {'ODD':o,'EVEN':e}
+
+print(solution([1, 2, 3, 5, 8, 9]))
+l=[1, 2, 3, 5, 8, 9]
+h=list(filter(lambda x:x%2==0,l))
+print(h)
